cc03d58a0712650667f5335ad644051063c44016
Zarrrinochka <1005534@bk.ru>
2021-05-04 22:03:48 +0700

    сложный блин слайдер

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,7 @@
 const input = document.querySelector('.slider__input')
 //const counter = document.querySelector('.slider__slide')
-const first = document.querySelector('.example__img-fat')
-const second = document.querySelector('.example__img-notfat')
+const first = document.querySelector('.example__img-skinny')
+const second = document.querySelector('.example__img-fat')
 
 input.addEventListener('input', function(e) {
   const value = e.target.value;

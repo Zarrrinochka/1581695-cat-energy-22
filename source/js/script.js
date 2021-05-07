b7825843b5a87c6cf0ccf4ab6d9af4ae6c876958
Zarrrinochka <1005534@bk.ru>
2021-05-08 00:03:32 +0700

    ind 1

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,17 @@
+let menu = document.querySelector('.menu');
+let imgx = document.querySelector('.img-x');
+let imgburger = document.querySelector('.img-burger');
+let toggle = document.querySelector('.toggle');
+
+toggle.onclick = function() {
+  menu.classList.toggle('block');
+  menu.classList.toggle('none');
+  imgx.classList.toggle('block');
+  imgx.classList.toggle('none');
+  imgburger.classList.toggle('none');
+  imgburger.classList.toggle('block');
+};
+
 const input = document.querySelector('.slider__input')
 //const counter = document.querySelector('.slider__slide')
 const first = document.querySelector('.example__img-skinny')
@@ -10,3 +24,4 @@ input.addEventListener('input', function(e) {
   first.style.width = value + '%'
   second.style.width = (100 - value) + '%'
 })
+

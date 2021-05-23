cf981882949897e4d5a0374332eda7af26bca930
Zarina <1005534@bk.ru>
2021-05-23 18:35:10 +0700

    готовенько

--- source/js/cats.js
+++ source/js/cats.js
@@ -1,9 +1,12 @@
 const input = document.querySelector('.slider__input')
-const first = document.querySelector('.example__img-skinny')
-const second = document.querySelector('.example__img-fat')
 
-input.addEventListener('input', function(e) {
-  const value = e.target.value;
-  first.style.width = value + '%';
-  second.style.width = (100 - value) + '%';
-});
+if (input) {
+  const first = document.querySelector('.example__img-skinny')
+  const second = document.querySelector('.example__img-fat')
+
+  input.addEventListener('input', function(e) {
+    const value = e.target.value;
+    first.style.width = value + '%';
+    second.style.width = (100 - value) + '%';
+  });
+};
